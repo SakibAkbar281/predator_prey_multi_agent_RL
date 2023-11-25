7e9cfb07f3df17932b6f9b4abab8ed8eb51f8417
SakibAkbar281 <sakibakbar281@gmail.com>
2023-11-25 08:36:40 -0500

    updates

--- main.py
+++ main.py
@@ -21,20 +21,23 @@ n_tigers = 2
 n_deers = 2
 env = Env(ground=ground)
 env.add(n_tigers, n_deers)
-env.load(tiger_q_file='tiger_q.pkl',deer_q_file='deer_q.pkl')
+
 
 # Texts
 deer_win_text = Text("Deer win")
 tiger_win_text = Text("Tigers win")
 
 # Training
-num_episodes = 100
+num_episodes = 10000
 num_steps = N_STEPS
-
+env.load(tiger_q_file='tiger_q.pkl',deer_q_file='deer_q.pkl')
 env.training(num_episodes, num_steps)
 env.save()
+
+# display aggregate scores
 tiger_scores = 0
 deer_scores = 0
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -42,7 +45,6 @@ while True:
             exit()
     screen.blit(ground(), dest=(0, 0))
     tiger_reward_list, deer_reward_list = env.transition()
-    # print(tiger_reward_list, deer_reward_list)
     tiger_scores += sum(tiger_reward_list)
     deer_scores += sum(deer_reward_list)
 
